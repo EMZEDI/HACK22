5479966140256afeee8b947d8b2dd9c9e77da9b6
Shidan <shidan.javaheri@mail.mcgill.ca>
2022-01-22 20:47:17 -0500

    The Finalized Optimization Algorithm! Now with efficiency 5.35!

--- Pywork/solution.py
+++ Pywork/solution.py
@@ -1,5 +1,50 @@
 from structure import Train, csv
 
+# function to optimize
+def optimize():
+    # loop to check the best time for a train to leave to pick up as many ppl as possible
+    availableTimes = [minute for minute in range(0, 178)]  # this is in minutes
+    unavailableTimes = []
+    trainTypes = [200, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 200, 200]
+    trainSchedule = []
+
+    # for all 16 trains
+
+    for type in trainTypes:
+        maxEncountered = 0
+        bestTime = 0
+        for time in availableTimes:
+            train = Train(type, time)
+            if time in unavailableTimes:
+                continue
+            else:
+                ppl_encountered = train.encounter()
+
+                if ppl_encountered > maxEncountered:
+                    maxEncountered = ppl_encountered
+                    bestTime = time
+
+        # append this best time to a list of unavailable times
+        unavailableTimes.append(bestTime)
+        unavailableTimes.append(bestTime - 1)
+        unavailableTimes.append(bestTime - 2)
+        unavailableTimes.append(bestTime + 1)
+        unavailableTimes.append(bestTime + 2)
+
+
+        finalTrain = Train(type, bestTime)
+        finalTrain.run_train_omptimize()
+        trainSchedule.append((type, bestTime))
+
+        # run the train and remove the passengers it can take
+
+    #train that leaves at 180 is FIXED
+    trainSchedule.append((200, 180))
+    trainSchedule.sort(key=lambda x: x[1])
+    print(trainSchedule)
+
+    return trainSchedule
+
 
 # Function to write the CSV information for the given schedule
 def write_csv(trains: list) -> None:
@@ -27,52 +72,19 @@ def write_csv(trains: list) -> None:
             writer.writerow([i + 1] + train.details)
 
 
-# Everything after this is testing, here we need to find a way to optimize 'schedule'
+# function that outputs the csv file
+def makeSchedule():
 
-# Our schedule has the format where the first number represents the train capacity (4 x 200; 12 x 400)
-# and the second represents the departure time (minutes after 7AM)
-
-# This schedule currently reflects the one provided in the Google Drive file from RailVision
-
-schedule = [(400, 0), (400, 17), (400, 20), (200, 23), (400, 39), (400, 40), (400, 49), (400, 60), (400, 67), (400, 70), (200, 87), (400, 90), (400, 110), (400, 130), (200, 150), (200, 180)]
-
-trains = [Train(c, t) for c, t in schedule]
-av = Train.run_schedule(trains)
-
-"""
-# The following is a brute force way to find the best permutation of trains, given that they depart
-# 11 mins apart (except the last, departing at 10 AM)
-
-import itertools
-
-L4_indexes = itertools.combinations(range(16), r=4)
-perms = [[200 if i in comb else 400 for i in range(16)] for comb in L4_indexes]
-
-
-# Assume the first permutation is the best
-best = [Train(c, 180 if i == 15 else i * 11) for i, c in enumerate(perms[0])]
-min_wait = Train.run_schedule(best)
-
-
-for i, perm in enumerate(perms):
-
-    Train.total_passengers_collected = 0
-    Train.total_waiting_time = 0
-    Train.stations = Station.initialise_stations()
-
-    trains = [Train(c, 180 if i == 15 else i * 11) for i, c in enumerate(perm)]
-
+    schedule = [(400, 0), (400, 17), (400, 20), (400, 29), (200, 40), (400, 43), (400, 49), (400, 60), (400, 67), (400, 70), (200, 87), (400, 90), (400, 110), (400, 130), (200, 150), (200, 180)]
+    trains = [Train(c, t) for (c, t) in schedule]
     av = Train.run_schedule(trains)
 
-    if av < min_wait:
-        min_wait = av
-        best = trains
-"""
+    # Writing the CSV of the schedule
+    write_csv(trains)
 
+    # For checking
+    print(av)
+    print(Train.total_passengers_collected)
+    return schedule
 
-# Writing the CSV of the schedule
-write_csv(trains)
-
-# For checking
-print(av)
-print(Train.total_passengers_collected)
+makeSchedule()
