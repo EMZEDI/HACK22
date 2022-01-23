188679b18f770d6b371c09621924566323ab3da8
Shidan <shidan.javaheri@mail.mcgill.ca>
2022-01-23 00:09:38 -0500

    Final Implementation with No Permutations. Comments, cleaned up. CSV file format fixed. Possible code for permutation solution inclued.

--- Pywork/solution.py
+++ Pywork/solution.py
@@ -1,23 +1,36 @@
 from structure import Train, csv
 
+# function to optimize the train schedule
 
-# function to optimize
-def optimizer():
-    # loop to check the best time for a train to leave to pick up as many ppl as possible
+'''FUNDAMENTAL ASSUMPTION BEHIND ALGORITHM: To minimize the average wait time, trains should be sent at the time when
+they will enounter the most number of people. Encounter is defined as people who arrived at the station in the two most
+recent arrival time intervals'''
+
+def optimize():
+    # list of all possible train departure times
     availableTimes = [minute for minute in range(0, 178)]  # this is in minutes
+    # list of times unavailable for train departure: will be appended as each train departure time is set
     unavailableTimes = []
+
+    # list of train types that the algorithm will iterate through IF WE HAVE TIME: calcualate the average waiting
+    # time for all permutations of this list. For now, list order chosen by trial and error Only 15 trains in this
+    # list, as the last train must leave at 10:00 to make sure everyone is picked up
     trainTypes = [200, 400, 400, 400, 400, 400, 200, 400, 400, 400, 400, 400, 400, 400, 200]
+
+    # list of tuples, of parameters used to construct a train object. Tuple is (capacity, current_time)
     trainSchedule = []
 
-    # for all 16 trains
-
+    # for all 15 trains
     for type1 in trainTypes:
+
+        # loop through all available times, skip those that are not available
         maxEncountered = 0
         bestTime = 0
         for time in availableTimes:
             train = Train(type1, time)
             if time in unavailableTimes:
                 continue
+            # if time is available, check how many people a train would encounter if it left at that time
             else:
                 ppl_encountered = train.encounter()
 
@@ -25,7 +38,8 @@ def optimizer():
                     maxEncountered = ppl_encountered
                     bestTime = time
 
-        # append this best time to a list of unavailable times, as well as +- 2
+        # append this best time to a list of unavailable times, as well as +/- 2 of that time to avoid conflicts in
+        # the station
         unavailableTimes.append(bestTime)
         unavailableTimes.append(bestTime - 1)
         unavailableTimes.append(bestTime - 2)
@@ -36,19 +50,15 @@ def optimizer():
         finalTrain.run_train_optimize()
         trainSchedule.append((type1, bestTime))
 
-        # run the train and remove the passengers it can take
-
-    # train that leaves at 180 is FIXED
+    # train that leaves at 10:00 (time value of 180) is FIXED
     trainSchedule.append((200, 180))
     trainSchedule.sort(key=lambda x: x[1])
-    print(trainSchedule)
-
+    # returns the list of constructor parameters for all 16 trains
     return trainSchedule
-
 
 # Function to write the CSV information for the given schedule
 def write_csv(trains: list) -> None:
-    with open("output.csv", "w") as output:
+    with open("output.csv", "w", newline="") as output:
         writer = csv.writer(output, delimiter=",")
 
         writer.writerow([
@@ -71,10 +81,9 @@ def write_csv(trains: list) -> None:
         for i, train in enumerate(trains):
             writer.writerow([i + 1] + train.details)
 
-
 # function that outputs the csv file
 def makeSchedule():
-    schedule = optimizer()
+    schedule = optimize()
     trainsList = [Train(c, t) for (c, t) in schedule]
     av = Train.run_schedule(trainsList)
 
@@ -82,10 +91,12 @@ def makeSchedule():
     write_csv(trainsList)
 
     # For checking
-    print(av)
-    print(Train.total_passengers_collected)
+    # print(av)
+    # print(Train.total_passengers_collected)
     return av
 
+
+#PRELIMINATRY CODE TO RUN EVERY PERMUTATION OF THE TRAIN LIST, AND CALCULATE THE AVERAGE WAIT TIME FOR THAT
 
 """
 # The following is a brute force way to find the best permutation of trains, given that they depart
@@ -116,5 +127,6 @@ for i, perm in enumerate(perms):
         min_wait = av
         best = trains
 """
+
 x = makeSchedule()
-print(x)
+print("Please refer to the output.csv file for the complete train schedule. The average waiting time is", x, "s")
--- Pywork/structure.py
+++ Pywork/structure.py
@@ -2,7 +2,9 @@ import copy
 import csv
 import datetime
 
+'''--- OBJECT ORIENTED APPORACH---'''
 
+#Train Station Class
 class Station:
 
     def __init__(self, passengers: list, time_to_next: int) -> None:
@@ -30,10 +32,9 @@ class Station:
 
         return [cls(l, t) for l, t in [(a, 11), (b, 12), (c, 14)]]
 
-
 class Train:
     stations = Station.initialise_stations()  # A list of stations
-    optimize_copy = copy.deepcopy(stations)
+    optimize_copy = copy.deepcopy(stations) # A cloned list that can be manipulated without affecting the original
     total_waiting_time = 0  # The total time spent waiting for all passengers
     total_passengers_collected = 0  # The total amount of people that took the train
 
@@ -52,7 +53,7 @@ class Train:
 
             # Appending station's arrival time
             self.details.append((datetime.datetime(2000, 1, 1, 7, 0)
-                                 + datetime.timedelta(minutes=self.current_time)).strftime("%-I:%M"))
+                                 + datetime.timedelta(minutes=self.current_time)).strftime("%#I:%M"))
 
             # Appending available capacity
             self.details.append(self.remaining)
@@ -103,7 +104,7 @@ class Train:
 
         # Appending Union Station's arrival time, available capacity, and offloaded amount
         self.details.append((datetime.datetime(2000, 1, 1, 7, 0)
-                             + datetime.timedelta(minutes=self.current_time)).strftime("%-I:%M"))
+                             + datetime.timedelta(minutes=self.current_time)).strftime("%#I:%M"))
         self.details.append(self.remaining)
         self.details.append(self.capacity - self.remaining)
 
@@ -115,41 +116,53 @@ class Train:
 
         return Train.total_waiting_time / Train.total_passengers_collected
 
+    #'encounter' function: if a train were to leave at a certain time, calculates how many people it will
+    # encounter. An encounter is defined as those who arrived in the two most recent time windows.
     def encounter(self):
-        # mirror the run train function but only on self - calculates the number of encounter
+
+        # mirror the run train function but only on self - calculates the number of encounters
         passengers_encountered: int = 0
+        #iterate through stations in the cloned list
         for station in Train.optimize_copy:
+            #edge case before 7:10 am
             if self.current_time // 10 == 0:
                 passengers_encountered += station.passengers[0]
-
+            #edge case when time is after 10 am
             elif self.current_time > 180:
                 self.current_time = 180
+            #otherwise, normal case
             else:
+                #count the passengers in the two most recent arrival times
                 for i in range((self.current_time // 10) - 1, (self.current_time // 10) + 1):
                     passengers_encountered += station.passengers[i]
-
+            # advance time to the next stations
             self.current_time += station.time_to_next
 
         return passengers_encountered
 
+    # optimize function: runs the train at the best time, and removes all the passengers it can pick up from the
+    # list of passengers in the station list
     def run_train_optimize(self) -> None:
 
+        # starts off having taken no passengers
         passengers_taken: int = 0
-
+        # iterate through all stations in the cloned list
         for station in Train.optimize_copy:
-            # check for when time exceed 180
+
+            # if there is no space left in the train, break, it cannot carry any more people.
             if self.remaining == 0:
                 break
-
+            # check for the time being after 10am - edge case
             if self.current_time > 180:
                 self.current_time = 180
 
-            # check for when the time is below 10 minutes
+            # check for when the time is below 7:10 am
             if self.current_time // 10 == 0:
                 passengers_taken += station.passengers[0]
                 station.passengers[0] = 0
                 self.remaining -= passengers_taken
-
+            # otherwise normal case: check two most recent arrival times and remove as many people from them as
+            # the train can carry without filling
             else:
                 for i in range((self.current_time // 10) - 1, (self.current_time // 10) + 1):
                     # if there is no space for everyone
